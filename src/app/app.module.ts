9a6d822d9189532d5d775a4ac2931fe8836f336f
compile <compile@compiles-MacBook-Pro.local>
2019-10-27 22:41:55 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { Graficas1Component } from "./pages/graficas1/graficas1.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { AppRoutingModule } from "./app.routes";
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    PagesComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
